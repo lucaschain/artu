5ee53c01a2d1854f5e4a0b770018abbbba13a8c4
Lucas Chain <sou@lucaschain.com.br>
2021-01-30 16:13:53 -0300

    Dividindo niveis em capitulos

--- jogo/src/levels/index.ts
+++ jogo/src/levels/index.ts
@@ -1,13 +1,5 @@
-import { Level01 } from './level_01'
-import { Level02 } from './level_02'
-import { Level03 } from './level_03'
-import { Level04 } from './level_04'
-import { Level05 } from './level_05'
+import { Chapter01 } from './chapter01'
 
 export const LevelList = [
-  Level01,
-  Level02,
-  Level03,
-  Level04,
-  Level05,
+  ...Chapter01
 ]
