f8b82a5fba55677a2a9a67b4fc321a75eadecfc5
Lucas Chain <sou@lucaschain.com.br>
2021-02-16 22:34:17 -0300

    adicão de dois capítulos

--- jogo/src/levels/chapter02.ts
+++ jogo/src/levels/chapter02.ts
@@ -1,4 +1,5 @@
 import { Block, Gate, SpeechListener, Goal } from '../core/tile/type'
+import { Direction } from '../math/rotation'
 import { LevelConfiguration } from '../core/level_configuration'
 
 export const Chapter02: LevelConfiguration[] = [
@@ -34,6 +35,89 @@ export const Chapter02: LevelConfiguration[] = [
       new SpeechListener({x: 3, y: 1}, 'Podemos maltratar os animais?', 'não', 'gate-2'),
       new Block({x: 3, y: 2}),
       new Goal({x: 3, y: 3}),
+    ]
+  },
+  {
+    name: 'chapter_02_level_003',
+    label: 'pensar num nome',
+    availableInstructions: [
+      'move', 'say_yes', 'say_no', 'turn_right', 'turn_left'
+    ],
+    width: 6,
+    height: 5,
+    startPosition: { x: 5, y: 2 },
+    startDirection: Direction.WEST,
+    tiles: [
+      new SpeechListener({x: 0, y: 0}, 'responda sim', 'sim', 'gate-1'),
+      new Block({x: 0, y: 1}),
+      new Goal({x: 0, y: 2}),
+      new Gate({x: 0, y: 3}, 'gate-2', 'horizontal'),
+      new Block({x: 1, y: 1}),
+      new Block({x: 1, y: 2}),
+      new Block({x: 1, y: 3}),
+      new Block({x: 2, y: 1}),
+      new SpeechListener({x: 2, y: 2}, 'responda nao', 'não', 'gate-2'),
+      new Block({x: 2, y: 3}),
+      new Block({x: 3, y: 1}),
+      new Gate({x: 3, y: 2}, 'gate-1'),
+      new Block({x: 3, y: 3},),
+      new Block({x: 4, y: 1}),
+      new Block({x: 4, y: 3}),
+    ]
+  },
+  {
+    name: 'chapter_02_level_004',
+    label: 'Mais',
+    availableInstructions: [
+      'move', 'say_yes', 'say_no', 'turn_right', 'turn_left'
+    ],
+    width: 7,
+    height: 7,
+    startPosition: { x: 3, y: 3 },
+    startDirection: Direction.SOUTH,
+    tiles: [
+      new Block({x: 0, y: 0}),
+      new Block({x: 0, y: 1}),
+      new Block({x: 0, y: 2}),
+      new SpeechListener({x: 0, y: 3}, 'Maçã é fruta?', 'sim', 'gate-1'),
+      new Block({x: 0, y: 4}),
+      new Block({x: 0, y: 5}),
+      new Block({x: 0, y: 6}),
+      new Block({x: 1, y: 0}),
+      new Block({x: 1, y: 1}),
+      new Block({x: 1, y: 2}),
+      new Block({x: 1, y: 4}),
+      new Block({x: 1, y: 5}),
+      new Block({x: 1, y: 6}),
+      new Block({x: 2, y: 0}),
+      new Block({x: 2, y: 1}),
+      new Block({x: 2, y: 2}),
+      new Block({x: 2, y: 4}),
+      new Block({x: 2, y: 5}),
+      new Block({x: 2, y: 6}),
+      new SpeechListener({x: 3, y: 0}, 'Ainda existem dinossauros?', 'não', 'gate-2'),
+      new Gate({x: 3, y: 4}, 'gate-1', 'horizontal'),
+      new Gate({x: 3, y: 5}, 'gate-2', 'horizontal'),
+      new Gate({x: 3, y: 6}, 'gate-3', 'horizontal'),
+      new Block({x: 4, y: 0}),
+      new Block({x: 4, y: 1}),
+      new Block({x: 4, y: 2}),
+      new Block({x: 4, y: 4}),
+      new Block({x: 4, y: 5}),
+      new Goal({x: 4, y: 6}),
+      new Block({x: 5, y: 0}),
+      new Block({x: 5, y: 1}),
+      new Block({x: 5, y: 2}),
+      new Block({x: 5, y: 4}),
+      new Block({x: 5, y: 5}),
+      new Block({x: 5, y: 6}),
+      new Block({x: 6, y: 0}),
+      new Block({x: 6, y: 1}),
+      new Block({x: 6, y: 2}),
+      new SpeechListener({x: 6, y: 3}, 'Peixes sabem voar?', 'não', 'gate-3'),
+      new Block({x: 6, y: 4}),
+      new Block({x: 6, y: 5}),
+      new Block({x: 6, y: 6}),
     ]
   }
 ]
