6031f22afd420dd35a6d4870243819e9d516fdf4
Lucas Chain <sou@lucaschain.com.br>
2021-03-12 21:31:10 -0300

    corrigindo deploy do prototipo

--- institucional/build_scripts/build.sh
+++ institucional/build_scripts/build.sh
@@ -2,14 +2,20 @@
 
 set -xeo pipefail
 
+SCRIPT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+cd $SCRIPT_PATH/..
+
 hexo generate
 
 cd ../jogo
 yarn install && yarn build
 
-PROTOTIPO_PATH=./institucional/public/prototipo
 cd ..
+PROTOTIPO_PATH=./institucional/public/prototipo
 mkdir -p $PROTOTIPO_PATH
-cp jogo/dist/* $PROTOTIPO_PATH
+cp jogo/dist/bundle.js $PROTOTIPO_PATH/bundle.js
+cp jogo/dist/index.html $PROTOTIPO_PATH/pre.index.html
 
-cat $PROTOTIPO_PATH/index.html | sed -E 's/(bundle\.js)/\/prototipo\/\1/' > $PROTOTIPO_PATH/index.html
+cat $PROTOTIPO_PATH/pre.index.html | sed -E 's/(bundle\.js)/\/prototipo\/\1/' > $PROTOTIPO_PATH/index.html
+
+rm $PROTOTIPO_PATH/pre.index.html
