9fe5414a34b5f0356f6ea7ecb1f45ae206e87f20
Lucas Chain <sou@lucaschain.com.br>
2021-05-02 17:04:33 -0300

    adiciona capitulo 1, nivel 4 - soma

--- jogo/src/levels/index.ts
+++ jogo/src/levels/index.ts
@@ -1,9 +1,11 @@
 import { Chapter01 } from './chapter01'
 import { Chapter02 } from './chapter02'
 import { Chapter03 } from './chapter03'
+import { Chapter04 } from './chapter04'
 
 export const LevelList = [
   ...Chapter01,
   ...Chapter02,
   ...Chapter03,
+  ...Chapter04,
 ]
