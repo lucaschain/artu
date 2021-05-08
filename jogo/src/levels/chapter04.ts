da776f91703e7fe0b4eb628925553dba9deff07f
Lucas Chain <sou@lucaschain.com.br>
2021-05-07 22:13:25 -0300

    adiciona level 2 capitulo 4

--- jogo/src/levels/chapter04.ts
+++ jogo/src/levels/chapter04.ts
@@ -19,5 +19,29 @@ export const Chapter04: LevelConfiguration[] = [
       new SpeechListener({x: 3, y: 1}, 'Quanto é 2 + 3?', '5', 'gate-1'),
       new Gate({x: 3, y: 2}, 'gate-1', 'horizontal'),
     ]
+  },
+  {
+    name: 'chapter_04_level_002',
+    label: 'Subtrai?',
+    availableInstructions: ['move', 'turn_right', 'turn_left', 'read', 'say_memory', 'sum', 'subtract'],
+    width: 5,
+    height: 3,
+    startPosition: { x: 2, y: 2, },
+    startDirection: Direction.NORTH,
+    tiles: [
+      new Block({x: 0, y: 0}),
+      new Block({x: 0, y: 1}),
+      new Sign({x: 0, y: 2}, 6),
+      new Block({x: 1, y: 0}),
+      new Block({x: 1, y: 1}),
+      new Goal({x: 2, y: 0}),
+      new Gate({x: 2, y: 1}, 'gate-1', 'horizontal'),
+      new SpeechListener({x: 2, y: 2}, '6 - 2?', '4', 'gate-1'),
+      new Block({x: 3, y: 0}),
+      new Block({x: 3, y: 1}),
+      new Block({x: 4, y: 0}),
+      new Block({x: 4, y: 1}),
+      new Sign({x: 4, y: 2}, 2),
+    ]
   }
 ]
