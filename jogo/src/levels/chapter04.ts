06a7aa2f3beb6b33d1c6b882394824f09cd7cf14
Lucas Chain <sou@lucaschain.com.br>
2021-05-28 20:52:39 -0300

    adiciona niveis finais

--- jogo/src/levels/chapter04.ts
+++ jogo/src/levels/chapter04.ts
@@ -23,7 +23,7 @@ export const Chapter04: LevelConfiguration[] = [
   {
     name: 'chapter_04_level_002',
     label: 'Subtrai?',
-    availableInstructions: ['move', 'turn_right', 'turn_left', 'read', 'say_memory', 'sum', 'subtract'],
+    availableInstructions: ['move', 'turn_right', 'turn_left', 'read', 'say_memory', 'subtract'],
     width: 5,
     height: 3,
     startPosition: { x: 2, y: 2, },
@@ -42,6 +42,40 @@ export const Chapter04: LevelConfiguration[] = [
       new Block({x: 4, y: 0}),
       new Block({x: 4, y: 1}),
       new Sign({x: 4, y: 2}, 2),
+    ]
+  },
+  {
+    name: 'chapter_04_level_003',
+    label: 'Repita',
+    availableInstructions: ['move', 'read', 'say_memory', 'sum'],
+    width: 5,
+    height: 1,
+    startPosition: { x: 0, y: 0 },
+    startDirection: Direction.EAST,
+    tiles: [
+      new Sign({x: 1, y: 0}, 1),
+      new SpeechListener({x: 2, y: 0}, '1+1+1?', '3', 'gate-1'),
+      new Gate({x: 3, y: 0}, 'gate-1'),
+      new Goal({x: 4, y: 0}),
+    ]
+  },
+  {
+    name: 'chapter_04_level_004',
+    label: 'Final',
+    availableInstructions: ['move', 'turn_left', 'turn_right', 'read', 'say_memory', 'subtract'],
+    width: 5,
+    height: 2,
+    startPosition: { x: 3, y: 1 },
+    startDirection: Direction.NORTH,
+    tiles: [
+      new Goal({x: 0, y: 0}),
+      new Block({x: 0, y: 1}),
+      new Gate({x: 1, y: 0}, 'gate-1'),
+      new Block({x: 1, y: 1}),
+      new SpeechListener({x: 2, y: 0}, '60-3?', '57', 'gate-1'),
+      new Block({x: 2, y: 1}),
+      new Sign({x: 4, y: 0}, 60),
+      new Sign({x: 4, y: 1}, 1),
     ]
   }
 ]
